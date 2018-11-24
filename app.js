40bb67e2ff6c5c11e85fab1594ade5d801ac0bd5
rahulnpadalkar <rahulnpadalkar@gmail.com>
2018-11-24 13:49:48 +0530

    Intial Express Commit

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-//create a server object:
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'}); // http header
-    var url = req.url;
-    console.log(url)
-    var hello = {'response':'Hello World'}
-    if(url ==='/'){
-        res.write(JSON.stringify(hello))
-        res.end()
-    }
-    if(url ==='/mul'){
-        res.write('<h1>Multiplying </h1>'); //write a response
-        res.end(); //end the response
-    }
+app.get('/',(req,res)=>{
 
-}).listen(3000, function(){
-    console.log("server start at port 3000"); //the server object listens on port 3000
+    res.send('Hello World!');
+
 });
+
+app.listen('3000',()=>{console.log('Server started at 3000');})
